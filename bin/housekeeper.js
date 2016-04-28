5c0c511421cf7da5dd8671f6fdd1bf9479a04a8c
DavidAQ <DavidAQ>
2016-04-28 21:17:29 +0800

    daemon

--- bin/housekeeper.js
+++ bin/housekeeper.js
@@ -1,18 +1,59 @@
 #!/usr/bin/env node
+const fs = require('fs-promise');
+const path = require('path');
 
-require('../lib/cli');
+fs.exists('package.json').then(exists => {
+	if (exists) {
+		throw 'This is not supposed to run in a project directory, run this in a dedicated directory';
+	}
+	return fs.exists(path.join('housekeeper', 'package.json'));
+}).then(exists => {
+	if (exists) {
+		throw 'This is not supposed to run in a project directory, run this in a dedicated directory';
+	}
+	return fs.mkdir('housekeeper').catch(err => null);
+}).then(() => {
+	process.chdir('housekeeper');
+	switch(process.argv[2] || 'run') {
+	case 'start':
+		require('../lib/daemon-cli');
+		break;
+	case 'stop':
+		stop();
+		break;
+	case 'restart':
+		stop().then(() => require('../lib/daemon-cli'));
+		break;
+	case 'run':
+		if (!process.argv[2]) {
+			console.log('You may run `housekeeper start` to start daemonized.');
+			console.log('Run `housekeeper help` for more information.\n\n');
+		}
+		require('../lib/cli');
+		break;
+	default:
+		console.log('Run with the following command:');
+		console.log('\tstart:    start the daemon server');
+		console.log('\tstop:     stop the daemon server');
+		console.log('\trestart:  restart the daemon server');
+		console.log('\trun:      run directly');
+	}
+}).catch(err => {
+	console.log(err.stack || err.message || err);
+});
 
-// var cp = require('child_process');
-
-// var child;
-
-// function startProcess() {
-//     child = cp.fork('../lib/cli');
-//     child.on('close', code => {
-//         if (code) {
-//             setTimeout(startProcess, 500);
-//         }
-//     });
-// }
-
-// startProcess();
+function stop() {
+	return fs.readFile('pid', 'utf-8')
+		.then(pid => {
+			try {
+           		process.kill(pid - 0);
+				console.log('Process ' + pid + ' killed');
+			} catch(err) {
+				console.log('Process ' + pid + ' may not exist anymore');
+			}
+            return fs.unlink('pid').catch(err => null)
+		})
+		.catch(err => {
+			console.log('No pid file found');
+		})
+}
